a626f42184d9777f3c20da394c2dfc262136d8cc
your_nameAnhelina <bogdanangelina@gmail.com>
2025-08-22 13:09:28 +0300

    add

--- app/notes/Notes.client.tsx
+++ app/notes/Notes.client.tsx
@@ -1,4 +1,4 @@
-import css from "./App.module.css";
+import css from "./NotesPage.module.css";
 import { useState } from "react";
 import { fetchNotes } from "@/lib/api";
 import type { NotesHttpResponse } from "@/types/note";
